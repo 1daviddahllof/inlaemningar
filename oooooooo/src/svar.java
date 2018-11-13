9878b36bc8d0e844d728257b0dfdac7cbac62712
1daviddahllof <42764469+1daviddahllof@users.noreply.github.com>
2018-11-13 08:52:59 +0100

    ååååååå

--- oooooooo/src/svar.java
+++ oooooooo/src/svar.java
@@ -1,4 +1,18 @@
-
+import java.util.Scanner;
 public class svar {
-
+	//här ligger svaren på uppgifterna
+	public static void main(String[] args) {
+		Scanner scn = new Scanner (System.in);
+		System.out.println("fråga 1: "+methods.volumeToMassSolid(IRON, 0.08));//80 * 1/10^3
+		
+		System.out.println("fråga 2: "+methods.svtDistance(2.8, 60));
+		
+		
+		System.out.println("fråga 3:"+ 
+		enums.FluidTable.WATER.density*0.005*enums.FluidTable.WATER.heatCapacity+"/deltaT");
+		
+		System.out.println("fråga 4"+ methods.velocityToHeight(50)+1.8);
+		
+		
+	}
 }
