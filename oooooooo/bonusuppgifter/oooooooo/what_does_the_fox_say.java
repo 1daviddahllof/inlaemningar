acd60bc813fa86fe848573e73fbc9e9b91922598
1daviddahllof <42764469+1daviddahllof@users.noreply.github.com>
2018-12-10 20:33:41 +0100

    ärsh

---
+++ oooooooo/bonusuppgifter/oooooooo/what_does_the_fox_say.java
@@ -0,0 +1,45 @@
+package oooooooo;
+import java.util.Scanner;
+import java.util.ArrayList;
+public class what_does_the_fox_say {
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner (System.in);
+		int ö = scn.nextInt();
+		for (int x = 0; x<ö; x++) {
+			scn.nextLine();
+			String s = scn.nextLine();
+			ArrayList<String> go = new ArrayList<String>();
+			while (true) {
+				String kom = scn.nextLine();	
+				if (kom.contains("what does")) {
+					break;
+				}
+				go.add(kom);
+			}
+			for (String goat : go) {
+				String what = goat;
+				int l = 0;
+				for (int x2 = 0; x2<goat.length(); x2++) {
+					if (goat.charAt(x2)==' ') {
+						l++;
+					}
+					if (l==2) {
+						l=x2;
+						break;
+					}
+				}
+				goat = goat.substring(l, goat.length());
+				System.out.println(goat);
+				while (s.contains(goat)) {
+					int io = s.indexOf(goat);
+					int goatlength = goat.length();
+					s = s.substring(0, io)+s.substring(io+goatlength, s.length());
+				}
+			}
+			System.out.println(s);
+		}
+		
+	}
+
+}
