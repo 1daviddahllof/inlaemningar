664af0e99392fde5d836be4f3a8db73b8c9fcdcf
1daviddahllof <42764469+1daviddahllof@users.noreply.github.com>
2019-02-21 14:26:42 +0100

    tre i rad är bättre nu

--- oooooooo/(dåliga)spel/oooooooo/tre_i_rad.java
+++ oooooooo/(dåliga)spel/oooooooo/tre_i_rad.java
@@ -13,30 +13,31 @@ public class tre_i_rad {
 			}
 		}
 //	här fyller vi alla positionerna med T, för tom
-		
+
 		System.out.println("T|T|T 1 2 3");
 		System.out.println("_|_|_ ");
 		System.out.println("T|T|T 4 5 6");
 		System.out.println("_|_|_");
 		System.out.println("T|T|T 7 8 9");
 
+		int count = 0;
 		while (true) {
 // spelet fortsätter tills det når en break längre ner
-			
+
 			int inx, iny;
 			boolean odd;
-		
+
 			while (true) {
 // denna loopen når en break om imputen motsvarar en position på spelet som 
 // är T, för tom
-				
+
 				inx = scn.nextInt();
 				odd = false;
-				if (inx%2 != 0) {
+				if (inx % 2 != 0) {
 					odd = true;
 				}
 // vi kommer att behöva veta om talet är udda senare
-				
+
 				iny = 0;
 				while (inx > 3) {
 					inx -= 3;
@@ -45,16 +46,15 @@ public class tre_i_rad {
 				inx--;
 // här blir inputsiffran tvår kordinater som motsvarar positionen som spelaren
 // valde
-				
+
 				if (c[inx][iny] == 'T') {
 					break;
-				}
-				else {
+				} else {
 					System.out.println("den finns redan, ta någon annan");
 				}
 // om denna position är tom så stoppar loopen annars ger den dig ett 
 // felmedelande och börjar om
-				
+
 			}
 			c[inx][iny] = 'O';
 			for (int y = 0; y < 3; y++) {
@@ -65,124 +65,35 @@ public class tre_i_rad {
 			}
 			System.out.println();
 // nu placeras ett O på rätt plats och en aktuell spelplan visas
-			
-			boolean row = true;
-			for (int x = 0; x<3; x++) {
-				if (c[x][iny] != 'O') {
-					row = false;
-					break;
-				}
-			}
-			if (row) {
-				System.out.println("du är en vinnare eller nåt");
-				break;
-			}
-// om row är sann betyder det att datorn inte hittade något annat än ett O på
-// den horisontella axeln där du placerade O:et och du vinner
-			
-			row = true;
-			for (int y = 0; y<3; y++) {
-				if (c[inx][y] != 'O') {
-					row = false;
-					break;
-				}
-			}
-			if (row) {
-				System.out.println("du är en vinnare eller nåt");
-				break;
-			}
-// här händer samma sak fast för vertikala axeln
-			
-			if (odd) {
-// om talet är udda betyder det att det spelaren har valt en plats som kan vinna
-// diagonalt
-				
-				int count = 0;
-				for (int x = 1; x<3; x++) {
-					if (iny+x<3 && inx+x<3) {
-						if (c[inx+x][iny+x]=='O') {
-							count++;
-						}
-					}
-					if (iny-x>=0 && inx-x>=0) {
-						if (c[inx-x][iny-x]=='O') {
-							count++;
-						}
-					}
-				}
-				if (count == 2) {
-					System.out.println("du är en vinnare eller nåt");
-					break;
-				}
-// om vi lägger till ett tal på x och y eller tar bort talet fån x och y får vi
-// en position som ligger diagonalt till punkten x, y
-// i detta fallet är y axeln baklänges och därför blir denna diagonal från
-// vänstra hörnet högst upp till högra hörnet längst ner
-// vi glömmer att räkna O:et som vi redan vet om och testar om dom andra också är
-// O om den hittar två betyder det att det är tre i rad
-				
-				
-				count = 0;
-				for (int x = 1; x<3; x++) {
-					if (inx+x<3 && iny-x>=0) {
-						if (c[inx+x][iny-x]=='O') {
-							count++;
-						}
-					}
-					if (inx-x>=0 && iny+x<3) {
-						if (c[inx-x][iny+x]=='O') {
-							count++;
-						}
-					}
-				}
-				if (count == 2) {
-					System.out.println("du är en vinnare eller nåt");
-					break;
-				}
-			}
-// här är samma sak men med den paralella diagonalen som
 
-			boolean all = true;
-			for (int x = 0; x<3; x++) {
-				for (int y = 0; y<3; y++) {
-					if (c[x][y] == 'T') {
-						all = false;
-						break;
-					}
-				}
+			if (tre(c, odd, inx, iny, 'O')) {
+				System.out.println("du är en dålig vinnare");
 			}
-			if (all) {
-				System.out.println("nu blev det lika");
-				break;
-			}
-// här ser vi om det finns någon T kvar eller om spelet blev lika
-// jag kunde ha gjort det här genom att räkna drag och det skulle ha varit mer
-// effektivt, men jag ändrar det inte
-			
+
+// tre är en metod längst ner i koden som hitter tre i rad			
+
 			int rx, ry;
 			do {
-				rx = (int) (Math.random()*3);
-				ry = (int) (Math.random()*3);
-			} while (c[rx][ry]!='T');
+				rx = (int) (Math.random() * 3);
+				ry = (int) (Math.random() * 3);
+			} while (c[rx][ry] != 'T');
 // här hittar vi slumpmässiga kordinater och börjar om om platsen inte är tom
-			
-			int X = rx+1;
+
+			c[rx][ry] = 'X';
+			int X = rx + 1;
 			int ryt = ry;
-			while (ryt>0) {
+			while (ryt > 0) {
 				ryt--;
-				X+=3;
+				X += 3;
 			}
 			odd = false;
 // jag undrar hur nödvändigt det var att se om vi ska testa för
 // diagonal vinst på detta sätt, så här men aja
 
-// här under är massa kopierad kod som testar om X får tre i rad precis som
-// vi gjorde med O. Kanske skulle ha använt metoder
-			
-			if (X%2 != 0) {
+			if (X % 2 != 0) {
 				odd = true;
 			}
-			c[rx][ry] = 'X';
+			char find = 'X';
 			for (int y = 0; y < 3; y++) {
 				for (int x = 0; x < 3; x++) {
 					System.out.print(c[x][y]);
@@ -190,65 +101,90 @@ public class tre_i_rad {
 				System.out.println();
 			}
 			System.out.println();
-			row = true;
-			for (int x = 0; x<3; x++) {
-				if (c[x][ry] != 'X') {
-					row = false;
-					break;
-				}
-			}
-			if (row) {
+
+			if (tre(c, odd, rx, ry, find)) {
 				System.out.println("du är så dålig att det är bättre att slumpa");
-				break;
 			}
-			row = true;
-			for (int y = 0; y<3; y++) {
-				if (c[rx][y] != 'X') {
-					row = false;
-					break;
-				}
-			}
-			if (row) {
-				System.out.println("du är så dålig att det är bättre att slumpa");
-				break;
-			}
-			if (odd) {
-				int count = 0;
-				for (int x = 1; x<3; x++) {
-					if (ry+x<3 && rx+x<3) {
-						if (c[rx+x][ry+x]=='X') {
-							count++;
-						}
-					}
-					if (ry-x>=0 && rx-x>=0) {
-						if (c[rx-x][ry-x]=='X') {
-							count++;
-						}
-					}
-				}
-				if (count == 2) {
-					System.out.println("du är så dålig att det är bättre att slumpa");
-					break;
-				}
-				count = 0;
-				for (int x = 1; x<3; x++) {
-					if (rx+x<3 && ry-x>=0) {
-						if (c[rx+x][ry-x]=='X') {
-							count++;
-						}
-					}
-					if (rx-x>=0 && ry+x<3) {
-						if (c[rx-x][ry+x]=='X') {
-							count++;
-						}
-					}
-				}
-				if (count == 2) {
-					System.out.println("du är så dålig att det är bättre att slumpa");
-					break;
-				}
-			}
+
 		}
 	}
 
+	public static boolean tre(char[][] c, boolean odd, int x, int y, char find) {
+
+		// om row är sann betyder det att datorn inte hittade något annat än ett O på
+		// den horisontella axeln där du placerade O:et och du vinner
+
+		boolean row = true;
+		for (int x2 = 0; x2 < 3; x2++) {
+			if (c[x2][y] != find) {
+				row = false;
+				break;
+			}
+		}
+		if (row) {
+			return true;
+		}
+
+		// här händer samma sak fast för vertikala axeln
+
+		row = true;
+		for (int y2 = 0; y2 < 3; y2++) {
+			if (c[x][y2] != find) {
+				row = false;
+				break;
+			}
+		}
+		if (row) {
+			return true;
+		}
+
+// om talet är udda är det möjligt en diagonal vinst möjlig
+
+		if (odd) {
+
+// Om vi lägger till ett tal på x och y eller tar bort talet fån x och y får vi
+// en position som ligger diagonalt till punkten x, y,
+// i detta fallet är y axeln baklänges och därför blir denna diagonal från
+// vänstra hörnet högst upp till högra hörnet längst ner.
+// Vi glömmer att räkna O:et som vi redan vet om och testar om dom andra också är
+// O om den hittar två betyder det att det är tre i rad
+
+			int count = 0;
+			for (int x2 = 1; x2 < 3; x2++) {
+				if (y + x2 < 3 && x + x2 < 3) {
+					if (c[x + x2][y + x2] == find) {
+						count++;
+					}
+				}
+				if (y - x2 >= 0 && x - x2 >= 0) {
+					if (c[x - x2][y - x2] == find) {
+						count++;
+					}
+				}
+			}
+			if (count == 2) {
+				return true;
+			}
+
+			// här är samma sak men med den paralella diagonalen
+
+			count = 0;
+			for (int x2 = 1; x2 < 3; x2++) {
+				if (x + x2 < 3 && y - x2 >= 0) {
+					if (c[x + x2][y - x2] == find) {
+						count++;
+					}
+				}
+				if (x - x2 >= 0 && y + x2 < 3) {
+					if (c[x - x2][y + x2] == find) {
+						count++;
+					}
+				}
+			}
+			if (count == 2) {
+				return true;
+			}
+		}
+		return false;
+	}
 }
