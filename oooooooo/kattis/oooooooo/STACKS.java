3ee58a182b038035df6e9a0370a5936725fccb55
1daviddahllof <42764469+1daviddahllof@users.noreply.github.com>
2019-03-18 11:34:10 +0100

    Update STACKS.java

--- oooooooo/kattis/oooooooo/STACKS.java
+++ oooooooo/kattis/oooooooo/STACKS.java
@@ -13,7 +13,24 @@ public class STACKS {
 			st.add(scn.nextInt());
 		}
 		ArrayList<ArrayList<Integer>> actions = new ArrayList<ArrayList<Integer>>();
-		
+		for (int x = 1; x <= 3; x++) {
+			ArrayList<Integer> thing = new ArrayList<Integer>();
+			thing.add(x);
+			actions.add(thing);
+		}
+		int moves = 0;
+		while (true) {
+			moves++;
+			int opsie = actions.size();
+			for (int x = 0; x < opsie; x++) {
+				for (int z = 1; z <= 3; z++) {
+					ArrayList<Integer> t = actions.get(0);
+					t.add(z);
+					actions.add(t);
+				}
+				actions.remove(0);
+			}
+			
+		}
 	}
-
 }
