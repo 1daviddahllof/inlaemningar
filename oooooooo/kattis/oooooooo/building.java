01e1852e834fff3e8118b89b76b2d321d3d30382
1daviddahllof <42764469+1daviddahllof@users.noreply.github.com>
2019-03-18 11:03:12 +0100

    en

--- oooooooo/kattis/oooooooo/building.java
+++ oooooooo/kattis/oooooooo/building.java
@@ -1,5 +1,43 @@
 package oooooooo;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class building {
+
+	public static void main(String[] args) {
+
+		// koden läser np rader
+		// om det står namn entered och om denna person redan finns i "in" så skriver
+		// den ANOMALY
+		// annars lägger den till namn i "in"
+		// om det står namn exited utan att namn finns i "in" skrivs ANOMALY ut
+
+		Scanner scn = new Scanner(System.in);
+		int np = scn.nextInt() * 2;
+
+		ArrayList<String> in = new ArrayList<String>();
+		for (int x = 0; x < np; x++) {
+			System.out.println();
+			String ina = scn.next();
+			String name = scn.next();
+			if (ina.equals("exit")) {
+				System.out.print(name + " exited");
+				if (in.contains(name)) {
+					in.remove(in.indexOf(name));
+				} else {
+					System.out.print(" (ANOMALY)");
+				}
+			} else {
+				System.out.print(name + " entered");
+				if (in.contains(name)) {
+					System.out.print(" (ANOMALY)");
+				} else {
+					in.add(name);
+				}
+			}
+		}
+
+	}
 
 }
--- oooooooo/kattis/oooooooo/sträng.java
+++ oooooooo/kattis/oooooooo/sträng.java
@@ -1,5 +1,43 @@
 package oooooooo;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class sträng {
 
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		String s = scn.next();
+		ArrayList<String> po = new ArrayList<String>();
+
+// x i den första loopen är längden av stringen som testas
+// y i den andra loopen är startpunkten på den string som testas
+// x börjar litet och sen höjs, så att vi hittar de minsta stringarna som funkar först
+		for (int x = 1; x <= s.length(); x++) {
+			for (int y = 0; y <= s.length() - x; y++) {
+				String maybe = s.substring(y, y + x);
+				String temp = s;
+				while (temp.contains(maybe)) {
+					temp = temp.replaceAll(maybe, "");
+// om maybe finns i stringen tar vi bort stringen "maybe" tills det inte finns någon maybe i stringen
+// om vi inte har någon kvar vet vi att maybe kan skapa stringen s
+
+					if (temp.length() == 0) {
+						po.add(maybe);
+					}
+				}
+			}
+			if (po.size() > 0) {
+				break;
+			}
+		}
+		
+
+// po har alla möjliga svar och därför sorterar vi dom i alfabetsordning och tar
+// det första i fall vi måste välja mellan flera svar.
+		java.util.Collections.sort(po);
+		System.out.println(po.get(0));
+
+		scn.close();
+	}
 }
