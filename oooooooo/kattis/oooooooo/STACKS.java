64aa6eef27e6d6db4b1a4f62f43bd354bf775eaf
1daviddahllof <42764469+1daviddahllof@users.noreply.github.com>
2019-03-22 11:42:55 +0100

    den funkar nu oftast

--- oooooooo/kattis/oooooooo/STACKS.java
+++ oooooooo/kattis/oooooooo/STACKS.java
@@ -8,7 +8,7 @@ public class STACKS {
 
 	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
-		
+
 		int N = scn.nextInt();
 		ArrayList<Integer> st = new ArrayList<Integer>();
 		st.add(2);
@@ -16,117 +16,110 @@ public class STACKS {
 		for (int x = 1; x <= N; x++) {
 			st.add(scn.nextInt());
 		}
-		
+
 		ArrayList<ArrayList<Integer>> actions = new ArrayList<ArrayList<Integer>>();
+		ArrayList<ArrayList<Boolean>> act = new ArrayList<ArrayList<Boolean>>();
+		ArrayList<Boolean> s = new ArrayList<Boolean>();
+		s.add(false);
+		s.add(false);
+		s.add(false);
+		act.add(s);
 		actions.add(st);
 		int moves = 0;
-	    search: while (true) {
-	    	String o = scn.nextLine();
-	    	if (o.equals("1")) {
-	    		break;
-	    	}
+
+		search: while (true) {
 			moves++;
+
 			int opsie = actions.size();
 			for (int x = 0; x < opsie; x++) {
-				for (int g = 0; g < actions.get(0).size(); g++) {
-					System.out.print(actions.get(0).get(g) + " ");
-				}
-				System.out.println();
-				System.out.println();
 				
 				ArrayList<Integer> sn = new ArrayList<Integer>();
 				sn.addAll(actions.get(0));
+				ArrayList<Boolean> snot = new ArrayList<Boolean>();
+				snot.addAll(act.get(0));
 				int end = sn.size(), in = sn.get(0), fl = sn.get(1);
-				
-				if (in == end) {
-					sn.add(1);
-					sn.set(1, -1);
+				boolean actiona = snot.get(0), actionb = snot.get(1), actionc = snot.get(2);
+
+				if (!actiona) {
+					ArrayList<Boolean> snot2 = new ArrayList<Boolean>(Collections.nCopies(3, false));
 					ArrayList<Integer> sn2 = new ArrayList<Integer>();
 					sn2.addAll(sn);
-					actions.add(sn2);
 					
-				}
-				else if (!(fl == -1 && sn.get(in) < 1)) {
-					sn.set(in, sn.get(in) + fl);
-					sn.set(1, fl * -1);
-					ArrayList<Integer> sn2 = new ArrayList<Integer>();
-					sn2.addAll(sn);
-					actions.add(sn2);
+					if (in == end) {
 					
-				}
-				if (finish(sn)) {
-					break search;
-				}
-				for (int g = 0; g < sn.size(); g++) {
-					System.out.print(sn.get(g) + " ");
-				}
-				System.out.println();
-				sn.clear();
-				sn.addAll(actions.get(0));
-				
-				if (in != 2) {
-					sn.set(0, in - 1);
-					ArrayList<Integer> sn2 = new ArrayList<Integer>();
+						sn2.add(1);
+						sn2.set(1, -1);
+						snot2.set(0, true);
+						actions.add(sn2);
+						act.add(snot2);
+
+
+					} else if (!(fl == -1 && sn.get(in) < 1)) {
 					
-					sn2.addAll(sn);
-					actions.add(sn2);
-					
-				}
-				for (int g = 0; g < sn.size(); g++) {
-					System.out.print(sn.get(g) + " ");
-				}
-				System.out.println();
-				sn.clear();
-				sn.addAll(actions.get(0));
-				
-				if (in >= end - 1) {
-					if (in != end) {
-						if (fl == 1) {
-							sn.set(0, in + 1);
-							ArrayList<Integer> sn2 = new ArrayList<Integer>();
-							
-							sn2.addAll(sn);
-							actions.add(sn2);
-							
-						}
+						sn2.set(in, sn2.get(in) + fl);
+						sn2.set(1, fl * -1);
+						snot2.set(0, true);
+						actions.add(sn2);
+						act.add(snot2);
+		
 					}
-				} else {
-					sn.set(0, in + 1);
+					if (finish(sn2)) {
+						break search;
+					}
+				}
+				if (!actionb) {
+					if (in != 2) {
+						ArrayList<Boolean> snot2 = new ArrayList<Boolean>(Collections.nCopies(3, false));
+						ArrayList<Integer> sn2 = new ArrayList<Integer>();
+						sn2.addAll(sn);
+						sn2.set(0, in - 1);
+						snot2.set(2, true);
+						actions.add(sn2);
+						act.add(snot2);
+					}
+				}
+				if (!actionc) {
+					ArrayList<Boolean> snot2 = new ArrayList<Boolean>(Collections.nCopies(3, false));
 					ArrayList<Integer> sn2 = new ArrayList<Integer>();
-					
 					sn2.addAll(sn);
-					actions.add(sn2);
-					
+					if (in >= end - 1) {
+						if (in != end) {
+							if (fl == 1) {
+								sn2.set(0, in + 1);
+								snot2.set(1, true);
+								actions.add(sn2);
+								act.add(snot2);
+							}
+						}
+					} else {
+						sn2.set(0, in + 1);
+						snot2.set(1, true);
+						actions.add(sn2);
+						act.add(snot2);
+					}
 				}
-				for (int g = 0; g < sn.size(); g++) {
-					System.out.print(sn.get(g) + " ");
-				}
-				System.out.println();
+
 				actions.remove(0);
-				
+				act.remove(0);
 			}
-			
+
 		}
 		System.out.println(moves);
 	}
 
 	public static boolean finish(ArrayList<Integer> now) {
-		boolean done = true;
-		int that = 0;
-		int x = 2;
-		while (that == 0) {
-			that = now.get(x);
-			x++;
-		}
 		if (now.get(1) == 1) {
 			return false;
 		}
+		boolean done = true;
+		int that = now.get(2);
 		for (int u = 3; u < now.size(); u++) {
-			if (now.get(u) != that && now.get(u) != 0) {
+			if (now.get(u) != that) {
 				done = false;
 				break;
 			}
 		}
 		return done;
 	}
+	
 }
