12795beeb656174820e9a69bd4b26604fef8659f
1daviddahllof <42764469+1daviddahllof@users.noreply.github.com>
2019-09-04 12:02:52 +0200

    no sé

---
+++ oooooooo/repetition/oooooooo/porque.java
@@ -0,0 +1,11 @@
+package oooooooo;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class porque {
+	public static int[] bagatego(int[] Alemania) {
+		Arrays.sort(Alemania);
+		return Alemania;
+	}
+}
