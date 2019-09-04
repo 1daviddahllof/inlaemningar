ce9c9dd919a5b7b9bfab69610f0f992c673ff096
1daviddahllof <42764469+1daviddahllof@users.noreply.github.com>
2019-09-04 11:28:25 +0200

    Update bola.java

--- oooooooo/repetition/oooooooo/bola.java
+++ oooooooo/repetition/oooooooo/bola.java
@@ -1,10 +1,12 @@
 package oooooooo;
-
+import java.util.Scanner;
 public class bola {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scn = new Scanner(System.in);
+		System.out.print("damelo");
+		double doble = scn.nextDouble();
+		System.out.print(4 * Math.PI * doble / 3);
 	}
 
 }
