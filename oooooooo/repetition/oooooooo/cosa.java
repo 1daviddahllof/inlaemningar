04622aef5c5e64c3bae9fc5e039561754e2b56aa
1daviddahllof <42764469+1daviddahllof@users.noreply.github.com>
2019-09-18 12:07:22 +0200

    olvidé

--- oooooooo/repetition/oooooooo/cosa.java
+++ oooooooo/repetition/oooooooo/cosa.java
@@ -3,6 +3,10 @@ package oooooooo;
 
 public class cosa {
 
-	
+	public static void main(String[] args) {
+		
+		System.out.print(deberes_de_métodos.Cuerda("lo contrario"));
+	}
 
 }
+
--- oooooooo/repetition/oooooooo/deberes_de_métodos.java
+++ oooooooo/repetition/oooooooo/deberes_de_métodos.java
@@ -3,14 +3,25 @@ package oooooooo;
 import java.util.Arrays;
 
 public class deberes_de_métodos {
+	
 	static int suma = 0;
-	public static int la_suma(int[] números) {
+	public static int laSuma(int[] números) {
 		if (números.length > 0) {
 			suma += números[0];
 			números = Arrays.copyOfRange(números, 1, números.length);
-			la_suma(números);
+			laSuma(números);
 		}
 		return suma;
 	}
-
+	
+	static String atrás = "";
+	public static String Cuerda(String cuerda) {
+		int longitud = cuerda.length();
+		if (longitud > 0) {
+			atrás = cuerda.charAt(0) + atrás;
+			cuerda = cuerda.substring(1, longitud);
+			Cuerda(cuerda);
+		}
+		return atrás;
+	}
 }
