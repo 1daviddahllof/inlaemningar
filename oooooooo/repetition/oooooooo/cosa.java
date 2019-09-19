811164908e9785fed54e10c71834109fa10868ff
1daviddahllof <42764469+1daviddahllof@users.noreply.github.com>
2019-09-19 17:14:00 +0200

    jag gav upp och googlade svaret

--- oooooooo/repetition/oooooooo/cosa.java
+++ oooooooo/repetition/oooooooo/cosa.java
@@ -1,11 +1,21 @@
 package oooooooo;
 
+import java.util.Scanner;
 
 public class cosa {
+	
 
 	public static void main(String[] args) {
-		
-		System.out.print(deberes_de_métodos.Cuerda("lo contrario"));
+		Scanner sc = new Scanner (System.in);
+		String tres = "tres";
+		while (true) {
+			tres = deberes_de_métodos.reverse(tres);
+			System.out.print(tres);
+			
+			if (sc.next().equals("no")) {
+				break;
+			}
+		}
 	}
 
 }
--- oooooooo/repetition/oooooooo/deberes_de_métodos.java
+++ oooooooo/repetition/oooooooo/deberes_de_métodos.java
@@ -4,8 +4,9 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class deberes_de_métodos {
-	
+
 	static int suma = 0;
+
 	public static int laSuma(int[] números) {
 		if (números.length > 0) {
 			suma += números[0];
@@ -14,40 +15,51 @@ public class deberes_de_métodos {
 		}
 		return suma;
 	}
-	
-	static String atrás = "";
-	public static String Cuerda(String cuerda) {
-		int longitud = cuerda.length();
-		if (longitud > 0) {
-			atrás = cuerda.charAt(0) + atrás;
-			cuerda = cuerda.substring(1, longitud);
-			Cuerda(cuerda);
-		}
-		return atrás;
+
+	public static String reverse(String str) {
+	    if ((null == str) || (str.length() <= 1)) {
+	        return str;
+	    }
+	    return reverse(str.substring(1)) + str.charAt(0);
 	}
-	
+
 	public static Object[] objectos(Object... fin) {
 		ArrayList<Object> cosas = new ArrayList<Object>();
-		for(Object cosa : fin) {
+		for (Object cosa : fin) {
 			cosas.add(cosa);
 		}
 		return cosas.toArray();
 	}
+
+	static ArrayList<String> anagramas = new ArrayList<String>();
+
 	public static void anagramas(String palabra) {
-		
+
+	}
+
+	public static int métodoFactorialDeMierda(int entreda) {
+		int diosMío = 1;
+		while (entreda > 1) {
+			diosMío *= entreda;
+			entreda--;
+		}
+		return diosMío;
 	}
 }
+
 class caja {
 	Object[] matrizDeObjectos;
+
 	public caja(Object... fin) {
-		// jag kan inte hitta ett sätt att använda objectos metoden här så jag kopierade in den istället som du ser
+		// jag kan inte hitta ett sätt att använda objectos metoden här så jag kopierade
+		// in den istället som du ser
 		ArrayList<Object> cosas = new ArrayList<Object>();
-		for(Object cosa : fin) {
+		for (Object cosa : fin) {
 			cosas.add(cosa);
 		}
 		this.matrizDeObjectos = cosas.toArray();
 	}
-	
+
 	public boolean comparador(caja cajaUno, caja cajaDos) {
 		for (int i = 0; i < cajaUno.matrizDeObjectos.length; i++) {
 			for (int i2 = 0; i2 < cajaDos.matrizDeObjectos.length; i2++) {
@@ -57,9 +69,5 @@ class caja {
 			}
 		}
 		return false;
-	}	
+	}
 }
-	
-
-
-
